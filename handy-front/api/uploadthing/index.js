59f5fdbee8695da4f69f5d4dfbe8c28c942d4d4b
maria4s <mariamawit89@gmail.com>
2026-04-20 12:22:14 +0300

    rename to index

--- handy-front/api/uploadthing/index.js
+++ handy-front/api/uploadthing/index.js
@@ -1,18 +1,14 @@
 import { createRouteHandler } from "uploadthing/express";
 import { uploadRouter } from "./router.js";
 
-// Create the Uploadthing Express handler
 const handler = createRouteHandler({
   router: uploadRouter,
 });
 
-// Vercel requires the standard (req, res) signature
 export default async (req, res) => {
-  // Use 'await' instead of 'return' to prevent Vercel routing conflicts
   await handler(req, res);
 };
 
-// Tell Vercel NOT to parse the body
 export const config = {
   api: {
     bodyParser: false,
